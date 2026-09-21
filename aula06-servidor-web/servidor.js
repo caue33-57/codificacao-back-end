4cbeabd9d3880700e050abc1bef67112d5b195a6
adam castro <riuksilva72@gmail.com>
2026-09-21 19:12:19 -0300

    adicionar servidor web

---
+++ aula06-servidor-web/servidor.js
@@ -0,0 +1,38 @@
+import http from 'http';
+
+const servidorWeb = http.createServer((req, res) =>{
+    console.log(`[LOG]Metodo Recebido: ${req.method} | Rota: ${req.url}`);
+     
+    const cabecalhoPadrao ={
+        'X-Content-Type-Options': 'nosniff',
+        'X-Frame-Options': 'DENY',
+
+    };
+    
+    if (req.url ==='/status'){
+        res.writeHead(200,{
+            ...cabecalhoPadrao,
+            'Content-Type': 'application/json'
+    });
+     
+    res.end(JSON.stringify({
+        servidorWeb: 'Online'
+    }));
+    
+    }else {
+        res.writeHead(404,{
+            ...cabecalhoPadrao,
+            'content-type': 'application/json'
+     });
+    
+     res.end(JSON.stringify({
+        erro: 'pagina nao encontrada!'
+     }));
+    }
+});
+
+servidorWeb.listen(300, ()=>{
+
+    console.log('Servidor Web ativo!');
+    console.log('Porta: 3000');
+});
